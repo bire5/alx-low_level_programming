8d7d6aa46e2bf8621821ce630e2765bc5df231e4
bire5 <lifayamga@gmail.com>
2022-09-12 22:34:51 -0700

    Initial commit, positive, negative values

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,14 +5,11 @@
 #include <time.h>
 
 /**
- *
- *main - Entry point
- *
- ** Determine if number is positive or negative
- *
- ** Return: Always 0 (Success)
- *
- */
+*
+* main - Entry point
+*
+** Return: Always 0 (Success)
+*/
 
 int main(void)
 
@@ -25,7 +22,6 @@ int main(void)
 		printf("%d is positive\n", n);
 		
 	else if (n == 0)
-
 		printf("%d is zero\n", n);
 
 	else
