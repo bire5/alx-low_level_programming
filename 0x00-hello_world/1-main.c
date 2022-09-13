8ab2eff425e262d54e77b46aa0f12b053efa9c2c
bire5 <lifayamga@gmail.com>
2022-09-12 18:34:32 -0700

    Compiler

--- 0x00-hello_world/1-main.c
+++ 0x00-hello_world/1-main.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-
  * main - Entry point
-
  *
-
- * Return: Always 0 (Success)
-
+ * Return: Always 0 (success)
  */
-
-int main(void)
-
+int main(0)
 {
-
 	printf("Holberton School");
-
-	return (0);
-
+	return (0)
 }
