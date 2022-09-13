7377aba8d730da4be49fad9d21e22cc54e052e03
bire5 <lifayamga@gmail.com>
2022-09-12 23:04:37 -0700

    Initial commit, positive, negative values

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,14 +5,12 @@
 #include <time.h>
 
 /**
-*
 * main - Entry point
 *
-** Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 
 int main(void)
-
 {
 	int n;
 
@@ -20,12 +18,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-		
 	else if (n == 0)
 		printf("%d is zero\n", n);
-
 	else
 		printf("%d is negative\n", n);
-
 	return (0);
 }
