214487858e7e4b2181e5f39a15e3721c60760264
bire5 <lifayamga@gmail.com>
2022-09-12 20:54:39 -0700

    Print sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c,
+	     long long int d;
+	float e;
+
+	printf("Size of a char: %u bytes(s)\n", sizeof(a));
+	printf("Size of an int: %u bytes(s)\n", sizeof(b));
+	printf("Size of a long int: %u bytes(s)\n", sizeof(c));
+	printf("Size of a long long int: %u bytes(s)\n", sizeof(d));
+	printf("Size of a float: %u bytes(e)]n", sizeof(f));
+	return (0);
