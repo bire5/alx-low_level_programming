de2d766054071c5d77631dcad92414e9b99ec815
bire5 <lifayamga@gmail.com>
2022-09-12 19:30:29 -0700

    Use puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main entry point
+ * main - Entry point
  *
- * this will print a text out
- * 
- * Return : Always 0 (success)
- *
+ * Return: Always 0 (success)
  */
 int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
