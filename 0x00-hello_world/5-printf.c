c710a40fed71fdd8d640ea9b5091e846f50d7b46
bire5 <lifayamga@gmail.com>
2022-09-12 19:39:31 -0700

    Avoid using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#includ <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\");
+	returns (0);
+
