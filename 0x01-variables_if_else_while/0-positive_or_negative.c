30906cbfe3a691155c87929bbc4f86932e704bad
bire5 <lifayamga@gmail.com>
2022-09-12 22:28:09 -0700

    Initial commit, positive, negative values

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 
 /**
  *
- *  * main - Entry point
+ *main - Entry point
  *
  ** Determine if number is positive or negative
  *
@@ -17,24 +17,18 @@
 int main(void)
 
 {
-
-		int n;
-
-
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-
-
-		if (n > 0)
-
-			printf("%d is positive\n", n);
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
 		
-		else if (n == 0)
+	else if (n == 0)
 
-			printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 
-		else
-			printf("%d is negative\n", n);
+	else
+		printf("%d is negative\n", n);
 
-		return (0);
+	return (0);
 }
