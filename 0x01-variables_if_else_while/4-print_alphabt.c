70f6c0903471881e76393535ccf00d093182caf1
bire5 <lifayamga@gmail.com>
2022-09-12 23:21:43 -0700

    Lowercase and newline

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,13 @@
 
 int main(void)
 {
-	char low, x, y;
-	
-	x = 'x';
-	y = 'y';
+	char low, e, q;
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low != x && low != y)
+		if (low != e && low != q)
 			putchar(low);
 	}
 	putchar('\n');
