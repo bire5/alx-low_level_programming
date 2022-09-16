21c9825f29074e0df17564c48b01d176c2193899
bire5 <lifayamga@gmail.com>
2022-09-15 17:19:18 -0700

    Adding header file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - test function to see if a number is positive_or_negative
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
