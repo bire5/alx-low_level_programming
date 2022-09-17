aa5f7b9e4dc154eb54c2426b92e902a0d96dea2b
bire5 <lifayamga@gmail.com>
2022-09-16 20:42:47 -0700

    Add isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,17 @@
 
 /**
  * _isupper - a function that checks for uppercase character
- *@c: input
- * Return: 1 if uppercase, 0 for otherwise
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	if((c >= 'A') && (c <= 'Z'))
+	if ((c >= 'A') && (c <= 'Z'))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
