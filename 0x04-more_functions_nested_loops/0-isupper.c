4eda9998c146f98cc3f14b5b2fa676bce54a227f
bire5 <lifayamga@gmail.com>
2022-09-16 18:57:06 -0700

    Add header files and isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-#include <ctype.h>
+
 /**
  * isupper - this function checks whether a character is uppercase or lowe
  *
  * Return: 1 for uppercase and 0 for others
  */
 
-int main(int c)
+int _isupper(int c)
 {
-	char c;
 	
 	if(c >= 'A')
 		return (1);
-	else:
+	else
 		return (0);
        	return (0);
 }
