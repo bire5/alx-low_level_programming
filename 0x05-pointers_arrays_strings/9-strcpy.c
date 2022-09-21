4943a4c272e24bbab4d76b3a0aaef0729a329af6
bire5 <lifayamga@gmail.com>
2022-09-20 17:42:48 -0700

    String copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,11 +14,11 @@ char *_strcpy(char *dest, char *src)
 
 	while (dest[i] != '\0')
 		dest[i++] = '\0';
-		i = 0;
+	i = 0;
 	while (src[srcLength] != '\0')
 		srcLength++;
 	for (i = 0; i < srcLength; i++)
 			dest[i] = src[i];
-			dest[srcLength] = '\0';
+	dest[srcLength] = '\0';
 	return (dest);
 }
