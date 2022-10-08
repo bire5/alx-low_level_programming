09f27729d4708166b00a4e468605f3620ae5cf58
bire5 <lifayamga@gmail.com>
2022-10-07 22:53:44 -0700

    Checked malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.>
+#include <stdlib.h>
 #include <limits.h>
 
 /**
- * malloc_checked - A function that allocates memory using malloc.
- * @b: An unsigned input integer
- * Return: Apointer to allocated memory or NULL if it fails
+ * *malloc_checked - Function that allocates memory
+ * @b: parameter
+ * Return: Return the printer
  */
+
 void *malloc_checked(unsigned int b)
 {
 	char *a;
