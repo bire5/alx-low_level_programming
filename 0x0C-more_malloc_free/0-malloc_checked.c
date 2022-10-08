9b29bcd1c8e16973d5a91b091a6bd21ce1f5d1b0
bire5 <lifayamga@gmail.com>
2022-10-07 22:46:13 -0700

    Checked malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,13 +6,14 @@
  * @b: An unsigned input integer
  * Return: Apointer to allocated memory or NULL if it fails
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *s;
-	s = malloc(b);
+	char *a;
+	a = malloc(sizeof(char) * b);
 
-	if (s == NULL)
+	if (a == NULL)
+	{
 		exit(98);
-	return (s);
+	}
+	return (a);
 }
