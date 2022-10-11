24434810f28961d41c7d560d3f61fcc606c3ba78
bire5 <lifayamga@gmail.com>
2022-10-11 15:44:41 -0700

    A macro named size

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,12 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Clifford
+ * Desc: Header file that defines a macro named size
+ * 	as an abbreviation for the token 1024
+ */
 
 #define SIZE 1024
 
