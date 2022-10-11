21d404054f8ea784fe4af7735b73de6aa531b6a6
bire5 <lifayamga@gmail.com>
2022-10-11 15:24:43 -0700

    Sum macro

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+/*
+ * File: 4-sum.h
+ * Auth: Clifford
+ * Desc: Header file that defines a function_like_macro SUM(x,y)
+ */
 
 #define SUM(x, y) ((x) + (y))
 
