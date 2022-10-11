82dd776a711895f62ccfd38ad28ab3e0f2e947d1
bire5 <lifayamga@gmail.com>
2022-10-11 07:15:50 -0700

    Define struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
-#ifndef dog_h
-#define dog_h
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * File: dog.h
- * By: Clifford
- * Use: To define a new structure type dog
+ * Auth: Clifford
+ * Desc: Header file that defines a new structure type dog
  */
 
 /**
  * struct dog - a new type describing a dog
- * @name - name of the dog
- * @age - age of the dog
- * @owner - owner of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 struct dog
 {
