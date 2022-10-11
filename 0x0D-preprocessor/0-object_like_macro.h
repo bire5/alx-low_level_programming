62edc738aea3a37f4775badc1e54b37f2a3d29e7
bire5 <lifayamga@gmail.com>
2022-10-11 10:42:17 -0700

    Define macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: object_like_macro.h
+ * Auth: Clifford
+ * Desc: A header file to define a macro named Size
+ */
+#define Size 1024
+
+#endif
+
