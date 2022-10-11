7356c3885ea6b1ecb708418f0b1927e9f68dea54
bire5 <lifayamga@gmail.com>
2022-10-11 10:58:28 -0700

    Macro named pi

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: pi.h
+ * Auth: Clifford
+ * Desc: Header file to define a macro named pi 
+ 	which is an abbreviation of 3.14159265359
+ */
+#define PI 3.14159265359
+
+#endif
