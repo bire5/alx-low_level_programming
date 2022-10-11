cf39133c669cc75453cad308beba221d90ac594c
bire5 <lifayamga@gmail.com>
2022-10-11 15:32:14 -0700

    Print name of file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
-/**
- * main -Prints the name of this file
- * Return: End program
+/*
+ * File: 2-main.c
+ * Auth: CLifford
  */
 
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ * Return: Always 0.
+ */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
