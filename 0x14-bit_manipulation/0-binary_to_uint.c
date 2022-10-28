0fd49de8741c2a21cfc2f0f87de31058c2972492
bire5 <lifayamga@gmail.com>
2022-10-28 11:58:06 -0700

    Convert binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,68 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Else - the converted number
+ * _pow - function that return the
+ * value of x raised to the power of y
+ * @x: parameter to base
+ * @y: parameter of pow
+ * Return: End program
+ */
+
+int _pow(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		y--;
+		x = x * _pow(x, y);
+		return (x);
+	}
+	return (0);
+}
+
+/**
+ * binary_to_uint - function that converts a binary number to an
+ * unsigned int
+ * @b: Pointer with the string whit the number to convert
+ * Return: The number converted
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int sum = 0;
+	int i, x, n;
 
-	if (b == '\0')
-		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	if (b == NULL)
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		return (0);
 	}
-	return (num);
+	
+	n = strlen(b) - 1;
+
+	for (x = 0, i = 0; n >= x; n--)
+	{
+		switch (b[i])
+		{
+
+			case '1':
+
+				sum = sum + _pow(2, n);
+				i++;
+				break;
+			case '0':
+				sum = sum + 0;
+				i++;
+				break;
+			default:
+				return (0);
+		}
+	}
+	return (sum);
 }
